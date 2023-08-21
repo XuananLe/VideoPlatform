ea1f068b89c4270044494b371194730f85de6cda
xuananle <lexuanan18102004@gmail.com>
2023-08-21 18:56:17 +0700

    Adding multiple

--- VideoPlatform.API/Controllers/VideosController.cs
+++ VideoPlatform.API/Controllers/VideosController.cs
@@ -38,27 +38,56 @@ public class VideosController : ControllerBase
             return NotFound(video + " not found");
         }
 
-        return new FileStreamResult(System.IO.File.OpenRead(filePath), "image/png");
+        return new FileStreamResult(System.IO.File.OpenRead(filePath), "video/mp4");
     }
 
     [HttpPost("Upload")] // POST /api/Videos/Upload
     public async Task<IActionResult> Create(IFormFile video)
     {
-        if (video == null)
+        if (video == null || video.Length == 0)
         {
-            return BadRequest("File is null");
+            return Ok("Invalid file.");
         }
 
-        var mime = video.FileName.Split('.').Last();
-
-        var filename = GuidService.generateShortGUID() + "." + mime;
-        var savedPath = Path.Combine(_env.WebRootPath, filename);
-        
-        using (var fileStream = new FileStream(savedPath, FileMode.Create, FileAccess.Write))
+        var fileExtension = Path.GetExtension(video.FileName).ToLower();
+    
+        if (fileExtension != ".mp4")
         {
+            return Ok("File format is not supported. Please upload an mp4 video.");
+        }
+
+        var filename = $"{GuidService.generateShortGUID()}{fileExtension}";
+        var savedPath = Path.Combine(_env.WebRootPath, filename);
+
+        await using var fileStream = new FileStream(savedPath, FileMode.Create, FileAccess.Write);
+        await video.CopyToAsync(fileStream);
+
+        return Ok("File uploaded successfully.");
+    }
+
+    [HttpPost("UploadMultiple")] // POST /api/Videos/UploadMultiple
+    public async Task<IActionResult> CreateMultiple(IFormFileCollection fileCollection)
+    {
+        if (fileCollection.Count <= 0 || fileCollection == null)
+        {
+            return Ok("Please select at least one file.");
+        }
+        foreach(var video in fileCollection)
+        {
+            var fileExtension = Path.GetExtension(video.FileName).ToLower();
+    
+            if (fileExtension != ".mp4")
+            {
+                return Ok($"The file {video.FileName} is not an mp4 video.");
+            }
+            var filename = $"{GuidService.generateShortGUID()}{fileExtension}";
+            var savedPath = Path.Combine(_env.WebRootPath, filename);
+
+            await using var fileStream = new FileStream(savedPath, FileMode.Create, FileAccess.Write);
             await video.CopyToAsync(fileStream);
         }
 
-        return Ok("File uploaded successfully");
+        return Ok("All file uploaded successfully.");
     }
+
 }
